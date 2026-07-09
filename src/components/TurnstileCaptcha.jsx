6029274b9e0f65c8f57cd23e31631102e0a07609
Francoamador27 <francohugoamador@hotmail.com>
2026-07-09 07:35:48 -0300

    actualizacion login

--- src/components/TurnstileCaptcha.jsx
+++ src/components/TurnstileCaptcha.jsx
@@ -1,16 +1,18 @@
+import { forwardRef } from 'react';
 import { Turnstile } from '@marsidev/react-turnstile';
 
-const TurnstileCaptcha = ({ onVerify }) => {
+const TurnstileCaptcha = forwardRef(({ onVerify }, ref) => {
   const siteKey = import.meta.env.VITE_TURNSTILE_SITE_KEY;
 
   return (
     <Turnstile
+      ref={ref}
       siteKey={siteKey}
       onSuccess={(token) => {
         onVerify(token); // devuelve el token al componente padre
       }}
     />
   );
-};
+});
 
 export default TurnstileCaptcha;
